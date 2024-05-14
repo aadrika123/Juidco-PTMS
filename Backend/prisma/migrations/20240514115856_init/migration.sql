4d8e60537aea68c7811a4954f9eab11c3460a5ff
pandeyprity <pandeyprity33@gmail.com>
2024-05-14 19:01:53 +0530

    changes in receipt

--- Backend/prisma/migrations/20240514115856_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[adhar_no]` on the table `onBoardedConductorDetails` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "onBoardedConductorDetails_adhar_no_key" ON "onBoardedConductorDetails"("adhar_no");
