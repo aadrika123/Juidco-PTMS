7b058d5eb1641c76a92f2c6bc89767d2cd166586
sahil raj <sahil27raj@gmail.com>
2025-02-26 16:53:17 +0530

    revert the changes

--- Backend/src/component/juidcoPTMS/controller/receipt/receipt.services.ts
+++ Backend/src/component/juidcoPTMS/controller/receipt/receipt.services.ts
@@ -54,8 +54,7 @@ class ReceiptServices {
 
     try {
       const setTime = req.body.data.time.replace(":", "").padStart(4, "0");
-      console.log("setTime",setTime)
-      // req.body.data.receipt_no = "123445";
+      req.body.data.receipt_no = "123445";
       // req.body.data.date = new Date();
       req.body.data.time = setTime;
       // const receipt_No =
